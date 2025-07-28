1c8f6df7bc63913067df52cdf1e79f23ea709460
ItsEhsanMM <itsehsanmm@gmail.com>
2025-07-28 19:41:33 +0330

    feat!<NewRequestModal>: add animation and open and close utility

---
+++ src/components/pages/RollCall/OverTimeRequest/NewRequestModal.tsx
@@ -0,0 +1,236 @@
+import { Separator } from "@/components/ui/separator";
+import { Clock, X } from "lucide-react";
+import { useForm } from "react-hook-form";
+import type z from "zod";
+import { modalValidation } from "./modalValidation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { motion, AnimatePresence } from "framer-motion";
+import {
+   Form,
+   FormControl,
+   FormField,
+   FormItem,
+   FormLabel,
+   FormMessage,
+} from "@/components/ui/form";
+import {
+   Select,
+   SelectContent,
+   SelectItem,
+   SelectTrigger,
+   SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import type React from "react";
+
+interface NewRequestModalProps {
+   open: boolean;
+   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const NewRequestModal = ({ open, setOpen }: NewRequestModalProps) => {
+   const form = useForm<z.infer<typeof modalValidation>>({
+      resolver: zodResolver(modalValidation),
+      defaultValues: {
+         employee: "",
+         date: new Date(),
+         entryDate: "",
+         exitDate: "",
+         description: "",
+      },
+   });
+
+   const onSubmit = () => {
+      console.log("submit");
+   };
+
+   return (
+      <AnimatePresence>
+         {open && (
+            <motion.div
+               initial={{ opacity: 0 }}
+               animate={{ opacity: 1 }}
+               exit={{ opacity: 0 }}
+               className="bg-black/25 fixed grid inset-0 z-100 overflow-y-auto items-center justify-center"
+            >
+               <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: 50 }}
+                  transition={{ duration: 0.3, ease: "easeInOut" }}
+                  className="bg-white rounded-lg shadow-lg p-6  space-y-8"
+               >
+                  <div>
+                     <div className="flex justify-between ">
+                        <X
+                           className="cursor-pointer"
+                           onClick={() => setOpen((prev) => !prev)}
+                        />
+                        <div className="flex flex-col items-center justify-center text-center mb-4">
+                           <h1 className="text-cupurple text-xl ">
+                              افزودن درخواست اضافه کاری اطلاعات
+                           </h1>
+                           <span className="text-gray-700">
+                              برای افزودن این رکورد به اطلاعات زیر نیاز داریم
+                           </span>
+                        </div>
+                     </div>
+                     <Separator />
+                  </div>
+                  <Form {...form}>
+                     <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-2"
+                     >
+                        <FormField
+                           control={form.control}
+                           name="employee"
+                           render={({ field }) => (
+                              <FormItem className="w-full space-y-2">
+                                 <FormLabel className="text-lg">
+                                    کارمند
+                                    <span className="text-red-500">*</span>
+                                 </FormLabel>
+                                 <FormControl>
+                                    <Select
+                                       value={field.value}
+                                       onValueChange={field.onChange}
+                                       dir="rtl"
+                                    >
+                                       <SelectTrigger className="w-full min-h-12 ">
+                                          <SelectValue placeholder="کارمند" />
+                                       </SelectTrigger>
+
+                                       <SelectContent className="z-100">
+                                          {/* Options would be dynamically generated here */}
+                                          <SelectItem value="employee1">
+                                             کارمند 1
+                                          </SelectItem>
+                                          <SelectItem value="employee2">
+                                             کارمند 2
+                                          </SelectItem>
+                                          <SelectItem value="employee3">
+                                             کارمند 3
+                                          </SelectItem>
+                                       </SelectContent>
+                                    </Select>
+                                 </FormControl>
+                                 <FormMessage />
+                              </FormItem>
+                           )}
+                        />
+
+                        <FormField
+                           control={form.control}
+                           name="date"
+                           render={() => (
+                              <FormItem className="w-full space-y-2">
+                                 <FormLabel className="text-lg">
+                                    تاریخ
+                                    <span className="text-red-500">*</span>
+                                 </FormLabel>
+                                 <FormControl>
+                                    <Input
+                                       disabled
+                                       placeholder="کامپوننت تاریخ جایگزین شود"
+                                       className="w-full min-h-12 border rounded p-2"
+                                    />
+                                 </FormControl>
+                                 <FormMessage />
+                              </FormItem>
+                           )}
+                        />
+                        <div className="flex flex-col gap-x-5 md:flex-row space-y-2 md:space-y-0">
+                           <FormField
+                              control={form.control}
+                              name="entryDate"
+                              render={() => (
+                                 <FormItem className="w-full space-y-2">
+                                    <FormLabel className="text-lg">
+                                       زمان ورود
+                                       <span className="text-red-500">*</span>
+                                    </FormLabel>
+                                    <FormControl>
+                                       <div className="flex items-center">
+                                          <Input
+                                             disabled
+                                             placeholder="کامپوننت زمان جایگزین شود"
+                                             className="w-full border rounded p-2 min-h-12"
+                                          />
+                                          <div className="bg-bgBack size-12 flex items-center justify-center">
+                                             <Clock className="size-5" />
+                                          </div>
+                                       </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                 </FormItem>
+                              )}
+                           />
+
+                           <FormField
+                              control={form.control}
+                              name="exitDate"
+                              render={() => (
+                                 <FormItem className="w-full space-y-2">
+                                    <FormLabel className="text-lg">
+                                       زمان خروج
+                                       <span className="text-red-500">*</span>
+                                    </FormLabel>
+                                    <FormControl>
+                                       <div className="flex items-center">
+                                          <Input
+                                             disabled
+                                             placeholder="کامپوننت زمان جایگزین شود"
+                                             className="w-full border rounded p-2 min-h-12"
+                                          />
+                                          <div className="bg-bgBack size-12 flex items-center justify-center">
+                                             <Clock className="size-5" />
+                                          </div>
+                                       </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                 </FormItem>
+                              )}
+                           />
+                        </div>
+
+                        <FormField
+                           control={form.control}
+                           name="description"
+                           render={({ field }) => (
+                              <FormItem className="w-full space-y-2">
+                                 <FormLabel className="text-lg">
+                                    علت درخواست
+                                    <span className="text-red-500">*</span>
+                                 </FormLabel>
+                                 <FormControl>
+                                    <Textarea
+                                       {...field}
+                                       placeholder="علت درخواست"
+                                       className="w-full min-h-20! border rounded"
+                                    />
+                                 </FormControl>
+                                 <FormMessage />
+                              </FormItem>
+                           )}
+                        />
+                        <Separator className="my-8" />
+                        <div className="flex justify-start">
+                           <Button
+                              type="submit"
+                              className=" px-4 py-2 rounded text-lg h-12"
+                           >
+                              ارسال درخواست
+                           </Button>
+                        </div>
+                     </form>
+                  </Form>
+               </motion.div>
+            </motion.div>
+         )}
+      </AnimatePresence>
+   );
+};
+export default NewRequestModal;
