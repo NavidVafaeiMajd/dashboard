9be1b8fc7514dab97b3351bdf0c00f72f458a127
ItsEhsanMM <itsehsanmm@gmail.com>
2025-08-04 19:32:06 +0330

    feat: add readonly input to date picker

--- src/components/shared/DatePicker.tsx
+++ src/components/shared/DatePicker.tsx
@@ -2,26 +2,39 @@
 import DatePicker from "react-multi-date-picker";
 import persian from "react-date-object/calendars/persian";
 import persian_fa from "react-date-object/locales/persian_fa";
+import { Input } from "../ui/input";
 interface DatePickerProps {
-  value: Date | null;
-  onChange: (date: Date | null) => void;
-  placeholder?: string;
+   value: Date | null;
+   onChange: (date: Date | null) => void;
+   placeholder?: string;
 }
 
-const CuDatePicker: React.FC<DatePickerProps>  = ({ value, onChange, placeholder = "تاریخ" }) => {
-  return (
-    <DatePicker
-      value={value ? new Date(value) : ""}
-        onChange={(date: any) =>
-        onChange(date?.isValid ? date.toDate() : null)
-      }
-      format="YYYY/MM/DD"
-      calendar={persian}
-      locale={persian_fa}
-      calendarPosition="bottom-right"
-      placeholder={placeholder}
-    />
-  );
+const CuDatePicker: React.FC<DatePickerProps> = ({
+   value,
+   onChange,
+   placeholder = "تاریخ",
+}) => {
+   return (
+      <DatePicker
+         value={value ? new Date(value) : ""}
+         onChange={(date: any) =>
+            onChange(date?.isValid ? date.toDate() : null)
+         }
+         format="YYYY/MM/DD"
+         calendar={persian}
+         locale={persian_fa}
+         calendarPosition="bottom-right"
+         placeholder={placeholder}
+         render={(value, openCalendar) => (
+            <Input
+               value={value}
+               onClick={openCalendar}
+               className="cursor-pointer min-h-12"
+               readOnly
+            />
+         )}
+      />
+   );
 };
 
 export default CuDatePicker;
