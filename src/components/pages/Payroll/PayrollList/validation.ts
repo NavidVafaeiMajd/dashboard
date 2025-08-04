be8036f4a6037a129337a36f04604e130b2d5212
navid <vafaermn@gmail.com>
2025-08-04 17:08:10 +0330

    fix validation bugs

--- src/components/pages/Payroll/PayrollList/validation.ts
+++ src/components/pages/Payroll/PayrollList/validation.ts
@@ -2,5 +2,10 @@ import z from "zod";
 
 export const validation = z.object({
     employee: z.string(),
-    month: z.string(),
+    date: z
+        .any()
+        .refine((d: unknown) => d instanceof Date && !isNaN(d.getTime()), {
+            message: "تاریخ الزامی است و یا معتبر نیست",
+        }),
+
 })
