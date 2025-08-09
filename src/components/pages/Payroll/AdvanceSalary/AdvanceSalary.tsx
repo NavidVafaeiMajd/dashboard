05c8966c34c35a6fdd6fe08d4aaf4255d55b41cb
navid <vafaermn@gmail.com>
2025-08-09 20:14:11 +0330

    base of advansed salary form

--- src/components/pages/Payroll/AdvanceSalary/AdvanceSalary.tsx
+++ src/components/pages/Payroll/AdvanceSalary/AdvanceSalary.tsx
@@ -1,39 +1,49 @@
-// itsEhsanMM
-
-import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/shared/data-table";
 import { useEffect, useState } from "react";
-import { GoPlus } from "react-icons/go";
 import { columns } from "./columns";
 import { ADVANCE_SALARY_LIST } from "./const";
+import { GoPlus } from "react-icons/go";
+import FormCM from "./Form";
 
 const AdvanceSalary = () => {
-   useEffect(() => {
-      document.title = "واحد سازمانی";
-   }, []);
+  useEffect(() => {
+    document.title = "لیست همه مساعده حقوق";
+  });
 
-   const [accordion, setAccordion] = useState(false);
+  const [accordion, setAccordion] = useState(false);
 
-   return (
-      <div className="flex bg-bgBack flex-col justify-between gap-y-10">
-         <div className="flex  justify-between p-2 px-5 border-b-2 border-red-500 items-center">
-            <h2>لیست همه مساعده حقوق</h2>
-            <Button
-               onClick={() => setAccordion(!accordion)}
-               className="cart-header-btn flex bg-greenDark text-white items-center p-2 gap-2 rounded-sm cursor-pointer"
+  return (
+    <>
+      <FormCM accordion={accordion} setAccordion={setAccordion} />
+      <div className="flex flex-col w-full bg-bgBack rounded-md overflow-hidden shadow-md h-full mb-1">
+        <div className="flex justify-between p-2 px-5 border-b-2 border-red-500 items-center">
+          <h2>لیست همه مساعده حقوق</h2>
+          
+          <div className="flex gap-2">
+            <button
+              onClick={() => setAccordion(!accordion)}
+              className="cart-header-btn flex bg-greenDark text-white items-center p-2 gap-2 rounded-sm cursor-pointer"
             >
-               <GoPlus className="w-5 h-5" />
-               ثبت جدید
-            </Button>
-         </div>
+              <GoPlus className="w-5 h-5" />
+              ثبت جدید
+            </button>
+          </div>
+        </div>
 
-         <DataTable
-            columns={columns}
-            data={ADVANCE_SALARY_LIST}
-            searchableKeys={["employee", "amount", "installmentAmount"]}
-         />
+        <DataTable
+          columns={columns}
+          data={ADVANCE_SALARY_LIST}
+          searchableKeys={[
+            "name",
+            "bankName",
+            "bankBranch",
+            "nationalId",
+            "status",
+            "employeeName",
+          ]}
+        />
       </div>
-   );
+    </>
+  );
 };
-
 export default AdvanceSalary;
