483264494c91a5a411e51554f05cb9cbcdca68ea
navid <vafaermn@gmail.com>
2025-08-12 16:45:02 +0330

    style layout

--- src/components/pages/employeDetailse/employeDetailse.tsx
+++ src/components/pages/employeDetailse/employeDetailse.tsx
@@ -1,6 +1,7 @@
 import { useParams, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { IoDocumentTextOutline } from "react-icons/io5";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { IoIosArrowBack } from "react-icons/io";
 
 const EmployeDetailse = () => {
   const { id } = useParams();
@@ -8,18 +9,28 @@ const EmployeDetailse = () => {
 
   return (
     <>
-      <h1>our user id is : {id}</h1>
       <Tabs defaultValue="user">
         <TabsList className="flex flex-col">
-          <TabsTrigger value="user">User</TabsTrigger>
+                  <h1>our user id is : {id}</h1>
+                  <div>
+                      
+                  </div>
+
+          <TabsTrigger value="user">
+            <span className="flex gap-2 justify-center items-center">
+              <IoDocumentTextOutline className="w-7! h-7!"/>
+              User
+            </span>
+            <span>
+              <IoIosArrowBack className="w-7! h-7!" />
+            </span>
+          </TabsTrigger>
           <TabsTrigger value="settings">Settings</TabsTrigger>
         </TabsList>
         <TabsContent value="user">
-
           <p>User content goes here</p>
         </TabsContent>
         <TabsContent value="settings">
-
           <p>Settings content goes here</p>
         </TabsContent>
       </Tabs>
