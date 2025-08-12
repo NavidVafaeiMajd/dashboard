54211baab97137ac3d23fc6be97726eb98a1d1be
navid <vafaermn@gmail.com>
2025-08-12 12:25:05 +0330

    base of layout tabs

--- src/components/pages/employeDetailse/employeDetailse.tsx
+++ src/components/pages/employeDetailse/employeDetailse.tsx
@@ -1,15 +1,30 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 
 const EmployeDetailse = () => {
-    const { id } = useParams();
-    const navigate = useNavigate();
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    return (
-        <>
-            <h1>our user id is : {id}</h1>
-        </>
-    );
-}
- 
+  return (
+    <>
+      <h1>our user id is : {id}</h1>
+      <Tabs defaultValue="user">
+        <TabsList className="flex flex-col">
+          <TabsTrigger value="user">User</TabsTrigger>
+          <TabsTrigger value="settings">Settings</TabsTrigger>
+        </TabsList>
+        <TabsContent value="user">
+
+          <p>User content goes here</p>
+        </TabsContent>
+        <TabsContent value="settings">
+
+          <p>Settings content goes here</p>
+        </TabsContent>
+      </Tabs>
+    </>
+  );
+};
+
 export default EmployeDetailse;
