6006408fa02b13424ebc131507346477ec45d544
ItsEhsanMM <itsehsanmm@gmail.com>
2025-08-16 16:27:44 +0330

    feat: add table

--- src/components/shared/StarRating.tsx
+++ src/components/shared/StarRating.tsx
@@ -4,6 +4,7 @@ import "@smastrom/react-rating/style.css";
 
 interface Props {
    star: number;
+   readonly?: boolean;
 }
 
 const myStyles = {
@@ -12,13 +13,17 @@ const myStyles = {
    inactiveFillColor: "#fbf1a9",
 };
 
-const StarRating = ({ star }: Props) => {
+const StarRating = ({ star, readonly = false }: Props) => {
    return (
-      <Rating
-         value={star}
-         itemStyles={myStyles}
-         readOnly
-      />
+      <div className="flex max-w-50 min-h-20 justify-between items-center gap-x-2">
+         <h4 className="text-2xl font-bold text-yellow-300">{star}</h4>
+         <Rating
+            value={star}
+            itemStyles={myStyles}
+            className="w-full"
+            readOnly={readonly}
+         />
+      </div>
    );
 };
 export default StarRating;
