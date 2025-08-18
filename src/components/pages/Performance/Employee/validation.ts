b419f57c69c8ab1ea708fa328192080ef1c46db9
ItsEhsanMM <itsehsanmm@gmail.com>
2025-08-18 16:40:35 +0330

    feat<EmployeeRating>: add month pick for form

--- src/components/pages/Performance/Employee/validation.ts
+++ src/components/pages/Performance/Employee/validation.ts
@@ -3,5 +3,6 @@ import z from "zod";
 export const validation = z.object({
    name: z.string(),
    employee: z.string(),
+   date: z.date(),
    ratings: z.record(z.number().int().min(1).max(5)),
 });
