7bf20a93b4a988b78086dc60e1f1647c6d68f35c
navid <vafaermn@gmail.com>
2025-08-29 19:07:44 +0330

    change TrackGoals.tsx

--- src/components/pages/Performance/TrackGoals/validation.ts
+++ src/components/pages/Performance/TrackGoals/validation.ts
@@ -1,17 +1,9 @@
 import z from "zod";
 
 export const validation = z.object({
-   purposeType: z.string({
-      error: "الزامی می باشد!",
-   }),
-
-   subject: z.string({
-      error: "الزامی می باشد!",
-   }),
-   purpose: z.string({
-      error: "الزامی می باشد!",
-   }),
-
+   purposeType: z.string().min(1, "انواع هدف الزامی است"),  
+   subject: z.string().min(1, "موضوع الزامی است"),
+   purpose: z.string().min(1, "دستیابی به هدف الزامی است"),
    startDate: z.date(),
    endDate: z.date(),
    description: z.string().optional(),
