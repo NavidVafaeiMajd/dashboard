30bdc430f689e9fd011e46192fcc18fbf9a78d22
ItsEhsanMM <itsehsanmm@gmail.com>
2025-09-06 18:57:38 +0330

    feat<PerformanceDetails>:add star ratings

--- src/components/pages/Performance/PerformanceDetails/PerformanceDetails.tsx
+++ src/components/pages/Performance/PerformanceDetails/PerformanceDetails.tsx
@@ -56,6 +56,37 @@ const PerformanceDetails = () => {
                <div className="w-full p-4 mb-5 border-b-2 border-pink-400">
                   <h2 className="text-xl">جزییات عملکرد</h2>
                </div>
+
+               <div className="flex gap-x-5 px-5 py-2 flex-col md:flex-row md:justify-between">
+                  <div className="w-full ">
+                     <div className="bg-gray-300 font-medium w-full px-2 py-4">
+                        <span className="text-lg">شایستگی های فنی</span>
+                     </div>
+                     <div className="bg-green-200 px-2 py-4 w-full flex justify-between">
+                        <span className="w-full text-lg">نشانگر</span>
+                        <span className="w-full text-lg">تنظیم اندیکاتور</span>
+                     </div>
+
+                     <div className="flex justify-between items-center">
+                        <span className="text-lg w-full">تست فنی 1</span>
+                        <StarRating star={5} />
+                     </div>
+                  </div>
+                  <div className="w-full">
+                     <div className="bg-gray-300 font-medium w-full px-2 py-4">
+                        <span className="text-lg">شایستگی های سازمانی</span>
+                     </div>
+                     <div className="bg-green-200 px-2 py-4 w-full flex justify-between">
+                        <span className="w-full text-lg">نشانگر</span>
+                        <span className="w-full text-lg">تنظیم اندیکاتور</span>
+                     </div>
+
+                     <div className="flex justify-between items-center">
+                        <span className="text-lg w-full">تست سازمانی 1</span>
+                        <StarRating star={2} />
+                     </div>
+                  </div>
+               </div>
             </div>
          </div>
       </div>
