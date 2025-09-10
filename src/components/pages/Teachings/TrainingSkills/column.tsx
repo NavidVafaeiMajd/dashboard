910032727fd2b1fa36a276c1618066637cd95c77
navid <vafaermn@gmail.com>
2025-09-10 11:15:33 +0330

    detaile of TrainingSkills

--- src/components/pages/Teachings/TrainingSkills/column.tsx
+++ src/components/pages/Teachings/TrainingSkills/column.tsx
@@ -1,5 +1,8 @@
 import type { ColumnDef } from "@tanstack/react-table";
-
+import { EditDialog } from "@/components/shared/EditDialog";
+import { Form } from "@/components/shared/Form";
+import { z } from "zod";
+import { DeleteDialog } from "@/components/shared/DeleteDialog";
 export interface SkillsRank {
   skillsType: string;
   creatDate: string;
@@ -10,15 +13,39 @@ export interface SkillsRank {
 
 export  const columns: ColumnDef<SkillsRank>[] = [
     {
-      accessorKey: "accountType",
+      accessorKey: "skillsType",
       header: "مهارت اموزشی",
       cell: ({ row }) => <div className="text-right">{row.getValue("skillsType")}</div>,
     },
     {
-      accessorKey: "accountDate",
+      accessorKey: "creatDate",
       header: "تاریخ ایجاد",
       cell: ({ row }) => <div className="text-center">{row.getValue("creatDate")}</div>,
+  },
+  {
+    id: "actions",
+    header: "عملیات",
+    cell: () => {
+      return (
+        <div className="flex items-center gap-2">
+          <EditDialog
+            onSave={() => {}}
+          fields={<>
+            <Form.Input name="skillsType" label="مهارت اموزشی" required />
+          </>}
+          defaultValues={{
+            skillsType: "",
+          }}
+          schema={z.object({
+            skillsType: z.string().min(1, "مهارت اموزشی الزامی است"),
+          })}
+          />
+          <DeleteDialog onConfirm={() => {}} />
+        </div>
+      );
+
     },
+    }
    
   ];
 
