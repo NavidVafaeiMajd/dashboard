102a03609f8a0e4b38f0ec65431fde7236fc8775
navid <vafaermn@gmail.com>
2025-09-24 12:58:53 +0330

    add Attendances data get method

--- src/components/pages/RollCall/AttendanceList/AttendanceList.tsx
+++ src/components/pages/RollCall/AttendanceList/AttendanceList.tsx
@@ -1,22 +1,26 @@
 import { DataTable } from "@/components/shared/data-table";
 import { columns } from "./columns";
-import { ATTENDANCE_LIST } from "./const";
 import { useEffect } from "react";
 import Table from "@/components/shared/section/Table";
+import { useGetRowsToTable } from "@/hook/useGetRows";
 
 const AttendanceList = () => {
    useEffect(() => {
       document.title = "لیست حضور و غیاب";
    });
+
+   const fetchAttendances = () => useGetRowsToTable("attendances");
+
    return (
       <div>
          <Table
             Title="لیست همه واحدها"
             table={
                <DataTable
-                  columns={columns}
-                  data={ATTENDANCE_LIST}
-                  searchableKeys={["employee", "status"]}
+               columns={columns}
+               queryKey={["attendances"]}
+               queryFn={fetchAttendances}
+                  searchableKeys={["attendances", "status"]}
                />
             }
          />
