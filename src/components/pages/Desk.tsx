b2c25d721c9977a2b219d7de86ce4242ac6d7b14
navid <vafaermn@gmail.com>
2025-09-27 21:02:05 +0330

    base of desk

--- src/components/pages/Desk.tsx
+++ src/components/pages/Desk.tsx
@@ -1,25 +1,42 @@
 import FeedCart from "./Desk/FeedCard/FeedCard";
 import DepartmentWiseChart from "./Desk/Charts/DepartmentWiseChart";
 import StaffPositionChart from "./Desk/Charts/StaffPositionChart";
-import TicketStatusChart from "./Desk/Charts/TicketStatusChart";
-import TicketPriorityChart from "./Desk/Charts/TicketPriorityChart";
+// import TicketStatusChart from "./Desk/Charts/TicketStatusChart";
+// import TicketPriorityChart from "./Desk/Charts/TicketPriorityChart";
 import StaffAttendanceChart from "./Desk/Charts/StaffAttendanceChart";
 import { useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const Desk = () => {
   const title = "پیشخوان";
   useEffect(() => {
     document.title = title;
   }, []);
+    
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["organizationStats"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:8000/api/organization/stats");
+      if (!res.ok) {
+        throw new Error("Failed to fetch stats");
+      }
+      return res.json();
+    },
+  });
+
+  if (isLoading) return <p>در حال بارگذاری...</p>;
+  if (isError) return <p>خطایی رخ داده است</p>;
+
+    console.log(data)
   return (
     <>
       <div className="min-h-200 py-10">
-        <FeedCart />
+        <FeedCart total_employees ={data?.total_employees} />
         <div className="grid md:grid-cols-2 items-end mt-10 gap-10">
           <DepartmentWiseChart />
           <StaffPositionChart />
-          <TicketStatusChart />
-          <TicketPriorityChart />
+          {/* <TicketStatusChart />
+          <TicketPriorityChart /> */}
           <StaffAttendanceChart />
         </div>
       </div>
--- src/components/pages/Desk/FeedCard/FeedCard.tsx
+++ src/components/pages/Desk/FeedCard/FeedCard.tsx
@@ -1,30 +1,15 @@
 import { FaUserTie, FaUsers } from "react-icons/fa6";
-import { LuMessageCircleMore, LuMessageCircleOff } from "react-icons/lu";
 
-const FeedCart = () => {
+const FeedCart = ({total_employees} : {total_employees : number}) => {
     return (<>
-        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-10">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-2 gap-10">
                     <div className="flex gap-3 items-center rounded-sm shadow-md overflow-hidden desk-cart">
                         <div className="desk-icon"><FaUserTie className="w-10 h-10  bg-[#58B368] text-white! hover:bg-green-200 hover:text-[#24C96F]! transition ease-in-out box-content p-7" /></div>
                         <div className="flex flex-col gap-3 ">
-                            <span>2</span>
+                            <span>{total_employees}</span>
                             <span className="text-[#58B368]">مجموع کارمندان</span>
                         </div>
-                    </div>         
-                    <div className="flex gap-3 items-center rounded-sm shadow-md overflow-hidden desk-cart">
-                        <div className="desk-icon"><LuMessageCircleMore className="w-10 h-10  bg-[#3ec9d6] text-white! hover:bg-[#D0CDF7] hover:text-[#7267EF]! transition ease-in-out box-content p-7" /></div>
-                        <div className="flex flex-col gap-3">
-                            <span>6</span>
-                            <span className="text-[#3ec9d6]">باز کردن (تیکت)</span>
-                        </div>
-                    </div>     
-                    <div className="flex gap-3 items-center rounded-sm shadow-md overflow-hidden desk-cart">
-                        <div className="desk-icon"><LuMessageCircleOff className="w-10 h-10  bg-[#ffa21d] text-white! hover:bg-[#FBDFB8] hover:text-[#ffa21d]! transition ease-in-out box-content p-7" /></div>
-                        <div className="flex flex-col gap-3">
-                            <span>0</span>
-                            <span className="text-[#ffa21d]">بسته شده (تیکت)</span>
-                        </div>
-                    </div>     
+                    </div>          
                     <div className="flex gap-3 items-center rounded-sm shadow-md overflow-hidden desk-cart">
                         <div className="desk-icon"><FaUsers className="w-10 h-10  bg-[#FF6680] text-white! hover:bg-[#F5C5C6] hover:text-[#FF6680]! transition ease-in-out box-content p-7" /></div>
                         <div className="flex flex-col gap-3">
